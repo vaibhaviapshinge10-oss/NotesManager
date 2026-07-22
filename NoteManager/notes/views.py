9a70f21522010d6ebf6a32015a6c19daf578d70c
Vaibhavi <vaibhaviapshinge10@gmail.com>
2026-07-22 18:21:20 +0530

    Dashboard Created

--- NoteManager/notes/views.py
+++ NoteManager/notes/views.py
@@ -3,4 +3,4 @@ from .models import Note
 
 def note_list(request):
     notes = Note.objects.all().order_by('-created_at')
-    return render(request, 'notes/note_list.html', {'notes': notes})
+    return render(request, 'note_list.html', {'notes': notes})
