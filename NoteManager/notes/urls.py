bde7c6110f7d10cafbd3a7c1b6bd6ad1dc57fc59
Vaibhavi <vaibhaviapshinge10@gmail.com>
2026-07-22 21:48:35 +0530

    Implement user login

--- NoteManager/notes/urls.py
+++ NoteManager/notes/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("register/", views.register, name='register'),
+    path("register/", views.register, name="register"),
+    path("login/", views.login_user, name="login"),
 ]
--- NoteManager/notes/views.py
+++ NoteManager/notes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def home(request):
     return render(request, "home.html")
@@ -23,3 +25,18 @@ def register(request):
         form = RegisterForm()
 
     return render(request, "register.html", {"form": form})
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid username or password.")
+
+    return render(request, "login.html")
