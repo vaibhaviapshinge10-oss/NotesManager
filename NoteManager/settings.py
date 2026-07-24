f995bb3cfbd500ad064a775f923d83c8f90a9135
Vaibhavi <vaibhaviapshinge10@gmail.com>
2026-07-24 18:19:36 +0530

    Changed database

--- NoteManager/settings.py
+++ NoteManager/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'NoteManager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'notesmanager',
+        'USER': 'root',
+        'PASSWORD': 'abc123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
