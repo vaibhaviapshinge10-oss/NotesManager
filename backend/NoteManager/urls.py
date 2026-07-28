5d5c149e67c9b1030c0d4512b501c914a33cfb3f
Vaibhavi <vaibhaviapshinge10@gmail.com>
2026-07-28 19:33:34 +0530

    Add JWT authentication for login

--- backend/NoteManager/urls.py
+++ backend/NoteManager/urls.py
@@ -1,14 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from django.contrib import admin
-from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("notes.urls")),
-    
-    path("api/token", TokenObtainPairView.as_view()),
-path("api/token/refresh", TokenRefreshView.as_view()),
+    path("admin/", admin.site.urls),
+    path("", include("notes.urls")),
+
+    path("api/login", TokenObtainPairView.as_view(), name="login"),
+    path("api/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
 ]
